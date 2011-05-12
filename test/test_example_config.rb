f479bb191f745039564556825f18cd5c7b38afad
Mark Percival <mdp@twitter.com>
2011-05-11 20:20:44 -0700

    better example config

--- test/test_example_config.rb
+++ test/test_example_config.rb
@@ -1,62 +1,32 @@
 require 'test_helper'
-require 'test_config'
+
+DiviningRod::Mappings.define(:format => :html, :tags => [:foo]) do |map|
+    map.ua /First/, :name => 'First', :tags => [:first]
+    map.ua /SecondMatch/, :name => 'Second', :tags => [:second]
+    map.with_options :tags => :grouping1 do |with_options|
+      with_options.ua /ThirdMatch/, :name => "ThirdMatch", :tags => [:third_match]
+      with_options.ua /FourthMatch/, :name => "FourthMatch", :tags => [:fourth_match]
+    end
+    map.ua /FifthMatch/, :name => 'FifthMatch', :tags => [:fifth_match, :and_more]
+    map.ua /ThirdMatch/, :name => "Failure", :tags => [:failed] # Shouldn't get here
+
+    map.with_options :tags => :second_group do |second_group|
+      # No values assigned, should just be the second_group tag
+      second_group.ua /ThirdMatch/
+      second_group.ua /SixthMatch/
+    end
+
+    map.ua /Last/, :tags => :last
+
+    map.default :name => "Unknown", :tags => :unknown
+end
 
 def profile_ua(ua)
   DiviningRod::Profile.new(request_mock(:ua => ua))
 end
 
-
 class ExampleConfigTest < Test::Unit::TestCase
   context 'the example config' do
-
-    context "recognizing Apple devices" do
-
-      should "recognize an iPad" do
-        profile = profile_ua("Mozilla/5.0(iPad; U; CPU iPhone OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B314 Safari/531.21.10")
-        assert profile.ipad?
-      end
-
-      should "recognize an iPod touch 2.2.1" do
-        profile = profile_ua("Mozilla/5.0 (iPod; U; CPU iPhone OS 2_2_1 like Mac OS X; en-us) AppleWebKit/525.18.1 (KHTML, like Gecko) Version/3.1.1 Mobile/5H11 Safari/525.20")
-        assert profile.ipod?
-      end
-
-      should "recognize an iPod touch 3.1.2" do
-        profile = profile_ua("Mozilla/5.0 (iPod; U; CPU iPhone OS 3_1_2 like Mac OS X; en-us) AppleWebKit/528.18 (KHTML, like Gecko) Version/4.0 Mobile/7D11 Safari/528.16")
-        assert profile.ipod?
-      end
-
-      should "recognize an iPhone 3.1.2" do
-        profile = profile_ua("Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_1_2 like Mac OS X; en-us) AppleWebKit/528.18 (KHTML, like Gecko) Version/4.0 Mobile/7D11 Safari/528.16")
-        assert profile.iphone?
-      end
-
-      should "recognize an iPhone 2.1.2" do
-        profile = profile_ua("Mozilla/5.0 (iPhone; U; CPU iPhone OS 2_2_1 like Mac OS X; en-us) AppleWebKit/525.18.1 (KHTML, like Gecko) Version/3.1.1 Mobile/5H11 Safari/525.20")
-        assert profile.iphone?
-      end
-
-    end
-
-    context "recognizing Android devices" do
-
-      should "recognize a basic android" do
-        profile = profile_ua("Mozilla/5.0 (Linux; U; Android 1.0; en-us; dream) AppleWebKit/525.10+ (KHTML, like Gecko) Version/3.0.4 Mobile Safari/523.12.2")
-        assert profile.android?
-      end
-
-    end
-
-    context "recognizing desktop browsers" do
-
-      should "recognize Internet Explorer 10" do
-        profile = profile_ua("Mozilla/4.0 (compatible; MSIE 10.0; Windows NT 7.0)")
-        assert profile.desktop?
-        assert_equal profile.version, 10
-        assert profile.html5?
-      end
-
-    end
 
     context "with root definition options" do
       should "carry through" do
@@ -64,6 +34,26 @@ class ExampleConfigTest < Test::Unit::TestCase
         assert_equal "Unknown", profile.name
         assert_equal :html, profile.format
       end
+    end
+
+    context "after a with_options block" do
+      should "not affect subsequest matches" do
+        profile = profile_ua("Last")
+        assert_equal [:foo, :last], profile.tags
+      end
+    end
+
+    should "match with tags" do
+      profile = profile_ua("FifthMatch")
+      assert_equal "FifthMatch", profile.name
+      assert profile.and_more?
+    end
+
+    should "stop at the first match" do
+      profile = profile_ua("ThirdMatch")
+      assert profile.third_match?
+      assert profile.grouping1?
+      assert !profile.failed?
     end
 
 
